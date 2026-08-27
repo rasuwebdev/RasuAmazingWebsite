4b9d6dc05ab1194c8f396547ab155e342eee1521
rasuwebdev <apiwadaphysics.lk@gmail.com>
2026-08-28 02:32:02 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Rasu Pin Lock Web Site</title>
+    <title>Rasu's Amazing Web Site</title>
 
     <style>
         * {
